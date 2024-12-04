06dd093151f5ee22fe578ffd3176d939096e86c3
xCuzMe12 <rapematic12@gmail.com>
2024-12-04 11:47:26 +0100

    update za bazo

--- public/app.js
+++ public/app.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", event => {
 
     const app = firebase.app();
-    console.log(app)
-
+    displayAds();    
 
 })
 
 
 function googleLogin() {
-  console.log("aaaa")
   const provider = new firebase.auth.GoogleAuthProvider();
 
   firebase.auth().signInWithPopup(provider)
@@ -23,3 +21,43 @@ let loginBtn = document.getElementById("login-btn");
 loginBtn.addEventListener("click", e => {
   googleLogin();
 })
+
+
+function displayAds(filter=null) {
+  const mainGrid = document.getElementById("main-grid");
+  if (filter) {
+
+  }
+  else {
+    const db = firebase.firestore();
+
+    const postedAds = db.collection("items");
+
+    postedAds.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const itemData = doc.data();
+        
+        const owner = itemData.Owner;
+        const price = itemData.Price;
+        const name = itemData.Name;
+        const dateOfEntry = itemData['Date of entry'];
+
+        console.log(owner + " " + price + " " + name);
+
+        const adDiv = document.createElement("div");
+        adDiv.classList.add("ad-item");
+        adDiv.innerHTML = `
+          <h3>${name}</h3>
+          <p>Owner: ${owner}</p>
+          <p>Price: $${price}</p>
+          <p>Date of Entry: ${dateOfEntry}</p>
+        `;
+
+        mainGrid.appendChild(adDiv);
+
+      })
+
+  })
+
+  }
+}
